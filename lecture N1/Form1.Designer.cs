292b48626c28e27ce5e32c8143de3f004ffb5cf0
User <User@DESKTOP-K9J7TKF>
2023-12-16 12:31:29 +0400

    Add project files.

---
+++ lecture N1/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace lecture_N1
+{
+    partial class calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(calculator));
+            this.n2 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.udrisi = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.Button();
+            this.fontDialog7 = new System.Windows.Forms.FontDialog();
+            this.fontDialog8 = new System.Windows.Forms.FontDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.SeaGreen;
+            this.n2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n2.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(128, 111);
+            this.n2.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(98, 84);
+            this.n2.TabIndex = 0;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.SeaGreen;
+            this.n4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n4.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(12, 203);
+            this.n4.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(104, 84);
+            this.n4.TabIndex = 0;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_click);
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.SeaGreen;
+            this.n5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n5.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(128, 203);
+            this.n5.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(98, 84);
+            this.n5.TabIndex = 0;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_click);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.SeaGreen;
+            this.n3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n3.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(238, 111);
+            this.n3.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(99, 84);
+            this.n3.TabIndex = 0;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.SeaGreen;
+            this.n6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n6.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(238, 203);
+            this.n6.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(99, 84);
+            this.n6.TabIndex = 0;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.SeaGreen;
+            this.n7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n7.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(12, 295);
+            this.n7.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(104, 84);
+            this.n7.TabIndex = 0;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_click);
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.SeaGreen;
+            this.n8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n8.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.Location = new System.Drawing.Point(128, 295);
+            this.n8.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(98, 84);
+            this.n8.TabIndex = 0;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.SeaGreen;
+            this.n9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n9.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.Location = new System.Drawing.Point(238, 295);
+            this.n9.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(99, 84);
+            this.n9.TabIndex = 0;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_click);
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.SeaGreen;
+            this.n0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n0.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.Location = new System.Drawing.Point(12, 387);
+            this.n0.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(104, 84);
+            this.n0.TabIndex = 0;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.n0_click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.SeaGreen;
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button11.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button11.Location = new System.Drawing.Point(128, 387);
+            this.button11.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(98, 84);
+            this.button11.TabIndex = 0;
+            this.button11.Text = ".";
+            this.button11.UseMnemonic = false;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.wertili);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.SeaGreen;
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button12.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button12.Location = new System.Drawing.Point(238, 387);
+            this.button12.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 84);
+            this.button12.TabIndex = 0;
+            this.button12.Text = "C";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.c);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.SeaGreen;
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button13.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button13.Location = new System.Drawing.Point(349, 111);
+            this.button13.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 84);
+            this.button13.TabIndex = 1;
+            this.button13.Text = "×";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.gamravleba);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.SeaGreen;
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button14.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button14.Location = new System.Drawing.Point(349, 203);
+            this.button14.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 84);
+            this.button14.TabIndex = 2;
+            this.button14.Text = "÷";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.gayofa);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.SeaGreen;
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button15.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.ForeColor = System.Drawing.Color.Chartreuse;
+            this.button15.Location = new System.Drawing.Point(349, 295);
+            this.button15.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(100, 84);
+            this.button15.TabIndex = 3;
+            this.button15.Text = "+";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.pliusi);
+            // 
+            // udrisi
+            // 
+            this.udrisi.BackColor = System.Drawing.Color.SeaGreen;
+            this.udrisi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.udrisi.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.udrisi.ForeColor = System.Drawing.Color.Chartreuse;
+            this.udrisi.Location = new System.Drawing.Point(349, 387);
+            this.udrisi.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.udrisi.Name = "udrisi";
+            this.udrisi.Size = new System.Drawing.Size(100, 84);
+            this.udrisi.TabIndex = 4;
+            this.udrisi.Text = "=";
+            this.udrisi.UseVisualStyleBackColor = false;
+            this.udrisi.Click += new System.EventHandler(this.toloba);
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.SeaGreen;
+            this.n1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.n1.Font = new System.Drawing.Font("Showcard Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(12, 111);
+            this.n1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(104, 84);
+            this.n1.TabIndex = 5;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // fontDialog7
+            // 
+            this.fontDialog7.Color = System.Drawing.Color.IndianRed;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Green;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Jokerman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 92);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 490);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.udrisi);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n2);
+            this.Font = new System.Drawing.Font("Snap ITC", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.MinimumSize = new System.Drawing.Size(26, 60);
+            this.Name = "calculator";
+            this.Text = "CALCULATOR";
+            this.Load += new System.EventHandler(this.calculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button udrisi;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.FontDialog fontDialog7;
+        private System.Windows.Forms.FontDialog fontDialog8;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ lecture N1/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lecture_N1
+{
+    public partial class calculator : Form
+    {
+        public calculator()
+        {
+            InitializeComponent();
+        }
+        double number1, number2;
+        string Symbol;
+        private void n1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="0")
+            {
+                textBox1.Text="1";
+            }
+            else
+            {
+                textBox1.Text+= "1";
+            }
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void pliusi(object sender, EventArgs e)
+        {
+            Symbol = "pliusi";
+            number1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void wertili(object sender, EventArgs e)
+        {
+            textBox1.Text += ".";
+        }
+
+        private void n3_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void n4_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void n5_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void n6_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void n7_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void n8_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void n9_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void n0_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void gamravleba(object sender, EventArgs e)
+        {
+            Symbol = "gamravleba";
+            number1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void gayofa(object sender, EventArgs e)
+        {
+            Symbol = "gayofa";
+            number1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void gamotvla(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void c(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toloba(object sender, EventArgs e)
+        {
+            number2=Convert.ToDouble(textBox1.Text);
+            if(Symbol=="pliusi")
+            {
+                textBox1.Text=Convert.ToString(number1+number2);
+            }
+            if (Symbol == "gamravleba")
+            {
+                textBox1.Text = Convert.ToString(number1*number2);
+            }
+            if (Symbol == "gayofa")
+            {
+                textBox1.Text = Convert.ToString(number1/number2);
+            }
+           
+        }
+    }
+}
